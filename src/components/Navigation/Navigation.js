87a6ba846fa8a6edda52d32ce5c0a8318fec96ce
Wellington Chen <wellingtonchenucd@gmail.com>
2019-10-12 19:09:45 -0700

    indents

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,14 +4,14 @@ const Navigation = ({routeChange, isSignedIn})=> {
         if (isSignedIn){
             return (
                 <nav style={{display:'flex', justifyContent: 'flex-end'}}>
-                <p className='f3 link dim black underline pa3 pointer' onClick={() => routeChange('Signout')}>Sign Out</p>
+                    <p className='f3 link dim black underline pa3 pointer' onClick={() => routeChange('Signout')}>Sign Out</p>
                 </nav>
             );
         } else if (!isSignedIn) {
             return (
                 <nav style={{display:'flex', justifyContent: 'flex-end'}}>
-                <p className='f3 link dim black underline pa3 pointer' onClick={() => routeChange('Signout')}>Sign In</p>
-                <p className='f3 link dim black underline pa3 pointer' onClick={() => routeChange('Register')}>Register</p>
+                    <p className='f3 link dim black underline pa3 pointer' onClick={() => routeChange('Signout')}>Sign In</p>
+                    <p className='f3 link dim black underline pa3 pointer' onClick={() => routeChange('Register')}>Register</p>
                 </nav>
             )
         }
